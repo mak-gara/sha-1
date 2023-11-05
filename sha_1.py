716239dbd1ab173ca29c8c4bed8539bb023786c1
mak-gara <maksimgaragulia@gmail.com>
2023-11-05 18:33:39 +0200

    feat: implement method for padding number with zeros

--- sha_1.py
+++ sha_1.py
@@ -1,4 +1,10 @@
+import struct
+
+
 class SHA1:
+
+    def __init__(self, input_message: str) -> None:
+        self.input_message = bytes(input_message, "utf-8")
 
     @staticmethod
     def rotate(n, b):
@@ -16,3 +22,14 @@ class SHA1:
         """
 
         return ((n << b) | (n >> (32 - b))) & 0xFFFFFFFF
+
+    def pad_with_zeros(self) -> bytes:
+        """
+        Pads the input message with zeros so that padded_data has 512 bits.
+
+        :return: A sequence of bytes representing the input_message padded with zeros.
+        """
+
+        padding = b"\x80" + b"\x00" * (63 - (len(self.input_message) + 8) % 64)
+        padded_data = self.input_message + padding + struct.pack(">Q", 8 * len(self.input_message))
+        return padded_data
