90f6a22bc7c5572fb52f9fc248fe156c1dbb6f99
mak-gara <maksimgaragulia@gmail.com>
2023-11-05 19:03:31 +0200

    feat: implement method for hashing input message

--- sha_1.py
+++ sha_1.py
@@ -2,9 +2,13 @@ import struct
 
 
 class SHA1:
+    """
+    Performs hashing using the SHA-1 algorithm.
+    """
 
     def __init__(self, input_message: str) -> None:
         self.input_message = bytes(input_message, "utf-8")
+        self.h = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0]
 
     @staticmethod
     def rotate(n, b):
@@ -56,3 +60,56 @@ class SHA1:
         for i in range(16, 80):
             w[i] = self.rotate((w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16]), 1)
         return w
+
+    def hash(self) -> str:
+        """
+        Generates SHA-1 hash for the provided data.
+
+        The hash() method implements the Secure Hash Algorithm 1 (SHA-1) logic for
+        generating a hashed output from the given data. It follows the SHA-1 hashing
+        process by first padding the input data, splitting it into blocks, and
+        applying the defined operations on these blocks iteratively. The algorithm
+        iterates through each block, expanding it, and updating the state variables
+        according to the SHA-1 specifications.
+
+        :return: hash for input message.
+        """
+
+        self.padded_data = self.pad_with_zeros()
+        self.blocks = self.split_blocks()
+
+        for block in self.blocks:
+            expanded_block = self.expand_block(block)
+            a, b, c, d, e = self.h
+
+            for i in range(80):
+                if 0 <= i < 20:
+                    f = (b & c) | ((~b) & d)
+                    k = 0x5A827999
+                elif 20 <= i < 40:
+                    f = b ^ c ^ d
+                    k = 0x6ED9EBA1
+                elif 40 <= i < 60:
+                    f = (b & c) | (b & d) | (c & d)
+                    k = 0x8F1BBCDC
+                elif 60 <= i < 80:
+                    f = b ^ c ^ d
+                    k = 0xCA62C1D6
+
+                a, b, c, d, e = (
+                    self.rotate(a, 5) + f + e + k + expanded_block[i] & 0xFFFFFFFF,
+                    a,
+                    self.rotate(b, 30),
+                    c,
+                    d,
+                )
+
+            self.h = (
+                self.h[0] + a & 0xFFFFFFFF,
+                self.h[1] + b & 0xFFFFFFFF,
+                self.h[2] + c & 0xFFFFFFFF,
+                self.h[3] + d & 0xFFFFFFFF,
+                self.h[4] + e & 0xFFFFFFFF,
+            )
+
+        return ("{:08x}" * 5).format(*self.h)
