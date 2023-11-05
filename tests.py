2dc41c1dd075e51833c177e320bddc78fc9255b4
mak-gara <maksimgaragulia@gmail.com>
2023-11-05 23:17:55 +0200

    test: add measuring working time of hash function

--- tests.py
+++ tests.py
@@ -1,11 +1,21 @@
 import unittest
+import time
 from hashlib import sha1
 
 from sha_1 import SHA1
 
 
+def measure_time(func):
+    start_time = time.perf_counter()
+    result = func()
+    execution_time = time.perf_counter() - start_time
+    return result, execution_time
+
+
 class SHA1TestCase(unittest.TestCase):
     def test_hash(self):
+        """Hash match test with library implementation."""
+
         input_messages = [
             "Hello World",
             "Cryptography for Developers",
@@ -15,14 +25,14 @@ class SHA1TestCase(unittest.TestCase):
         ]
 
         for message in input_messages:
-            custom = SHA1(message).hash()
-            standard = sha1(bytes(message, "utf-8")).hexdigest()
+            custom, custom_exec_time = measure_time(SHA1(message).hash)
+            standard, standard_exec_time = measure_time(sha1(bytes(message, "utf-8")).hexdigest)
+
+            print(f"\n\nInput message: {message}")
+            print(f"Custom implementation:\t{custom} \tExecution time: {custom_exec_time}\tseconds")
+            print(f"Library implementation:\t{standard}\tExecution time: {standard_exec_time}\tseconds")
 
             self.assertEqual(custom, standard)
-
-            print(f"Input message: {message}\n"
-                  f"Custom implementation: {custom}\n"
-                  f"Standard: {standard}\n\n")
 
 
 if __name__ == '__main__':
